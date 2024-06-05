2b26032d9e81288a4554a541d05ea4be277f579f
Al Amin <khanalamin278@gmail.com>
2024-06-05 18:49:16 +0600

    first commit

--- src/test/java/org/example/AppiumBasics.java
+++ src/test/java/org/example/AppiumBasics.java
@@ -1,17 +1,17 @@
 package org.example;
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
-import org.testng.annotations.AfterClass;
+//import io.appium.java_client.AppiumDriver;
+//import io.appium.java_client.android.AndroidDriver;
+//import io.appium.java_client.android.options.UiAutomator2Options;
+//import io.appium.java_client.service.local.AppiumDriverLocalService;
+//import io.appium.java_client.service.local.AppiumServiceBuilder;
+//import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import resource.BaseText;
 
-import java.io.File;
+//import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URL;
+//import java.net.URL;
 
 
 public class AppiumBasics extends BaseText {
